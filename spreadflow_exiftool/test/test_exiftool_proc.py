1e74fc697d3d956ee93d406445fa32d03b05e3a6
Lorenz Schori <lo@znerol.ch>
2016-06-09 17:20:49 +0200

    Add default instance test

--- spreadflow_exiftool/test/test_exiftool_proc.py
+++ spreadflow_exiftool/test/test_exiftool_proc.py
@@ -8,4 +8,12 @@ from spreadflow_exiftool.proc import MetadataExtractor
 
 
 class SpreadflowExiftoolTestCase(unittest.TestCase):
-    pass
+    def test_default_instance(self):
+        proc = MetadataExtractor('exiftool')
+        expected_args = (b'-j', b'-charset', b'exiftool=UTF-8', b'-charset', b'filename=UTF-8')
+
+        self.assertEqual(proc.args, expected_args)
+        self.assertEqual(proc.attrib, 'metadata')
+        self.assertEqual(proc.buffersize, 2**22)
+        self.assertEqual(proc.decode_errors, 'strict')
+        self.assertEqual(proc.strport, 'exiftool')
